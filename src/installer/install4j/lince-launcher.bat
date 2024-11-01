f16a15bd43fedc5760636f9eb19fb750c16e508d
Alberto Soto Fernandez <albert.soto@gmail.com>
2024-11-01 19:32:25 +0100

    bring back installer materials

---
+++ src/installer/install4j/lince-launcher.bat
@@ -0,0 +1,2 @@
+start javaw -jar -Xms1024m -Xmx1024m lince-desktop.jar --add-exports javafx.graphics/com.sun.javafx.application=ALL-UNNAME --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.util.concurrent=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.locks=ALL-UNNAMED --add-exports=jdk.unsupported/sun.misc=ALL-UNNAMED --add-exports=java.base/sun.nio.ch=ALL-UNNAMED
+:: java -jar lince-desktop.jar
