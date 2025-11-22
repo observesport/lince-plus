01573cf4b2aaaf555a9a882606eab2b554d39bc6
Alberto Soto Fernandez <albert.soto@gmail.com>
2025-11-22 11:31:35 +0100

    pre bundle contains

--- src/lince-desktop/src/main/resources/public/assets/___vite-browser-external_commonjs-proxy-DSc3qcS5.js
+++
@@ -1,1 +0,0 @@
-import{cK as e}from"./index-DdjfOgen.js";const t={},r=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),a=e(r);export{a as r};
--- src/lince-desktop/src/main/resources/public/assets/index-a2513857.js
+++
@@ -1,1 +0,0 @@
-import{c9 as t,ca as a,cb as e,cc as r,cd as c}from"./index-b2960181.js";const o=t;o.Header=a;o.Footer=e;o.Content=r;o.Sider=c;const s=o;export{s as L};
