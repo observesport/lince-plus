bf43048915c6326c82445fc1c897befc857512bd
Alberto Soto Fernandez <albert.soto@gmail.com>
2026-02-25 06:01:28 +0100

    update frontend with 4.0.4 version

--- src/lince-desktop/src/main/resources/public/assets/___vite-browser-external_commonjs-proxy-CD5bKA9w.js
+++
@@ -1,1 +0,0 @@
-import{cX as e}from"./index-aI5OvWDK.js";const t={},r=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),a=e(r);export{a as r};
---
+++ src/lince-desktop/src/main/resources/public/assets/___vite-browser-external_commonjs-proxy-CZEiLZKM.js
@@ -0,0 +1,1 @@
+import{cX as e}from"./index-dKctr3l9.js";const t={},r=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),a=e(r);export{a as r};
